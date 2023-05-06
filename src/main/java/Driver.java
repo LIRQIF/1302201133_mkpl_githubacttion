af567e87977e8194352abee169f044eab572d335
@WIFI.ID <67087250+LIRQIF@users.noreply.github.com>
2023-05-06 23:26:39 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,21 +1,35 @@
-import src.main.java.Counter;
+public class Counter {
+    private int count;
+    
+    public Counter() {
+        count = 0;
+    }
+    
+    public int getCount() {
+        return count;
+    }
+    
+    public void increment() {
+        count++;
+    }
+    
+    public void decrement() {
+        count--;
+    }
+}
 
 public class Driver {
-
-	public static void main(String[] args) {
-		
-		Counter counter = new Counter();
-		
-		System.out.println("Count:" + counter.getCount());
-		
-		counter.increment();
-		
-		System.out.println("Count:" + counter.getCount());
-		
-		counter.decrement();
-		
-		System.out.println("Count:" + counter.getCount());
-		
-	}
-
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        
+        System.out.println("Count: " + counter.getCount());
+        
+        counter.increment();
+        
+        System.out.println("Count: " + counter.getCount());
+        
+        counter.decrement();
+        
+        System.out.println("Count: " + counter.getCount());
+    }
 }
